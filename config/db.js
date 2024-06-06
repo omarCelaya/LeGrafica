dabf571f4dc38cf075ac02fd7e3ea87a7cdf42be
omarCelaya <omercadocelaya@gmail.com>
2024-06-06 07:49:55 -0700

    deploy

--- config/db.js
+++ config/db.js
@@ -1,18 +1,5 @@
 // // config/db.js
-// const mysql = require('mysql2');
-// const dotenv = require('dotenv');
 
-// dotenv.config();
-
-// const db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-// });
-
-// module.exports = db;
-// db.js
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
--- repositories/clienteRepository.js
+++ repositories/clienteRepository.js
@@ -1,17 +1,8 @@
 // repositories/clienteRepository.js
-const db = require('../config/db');
 const Usuario = require('../models/Usuario');
-const bcrypt = require('bcryptjs');
 
 exports.getClienteById = async (id) => {
     return new Promise(async (resolve, reject) => {
-        // db.query('SELECT id, nombre, email, password, phone FROM usuarios WHERE id = ?', [id], (err, results) => {
-        //     if (err) {
-        //         reject(err);
-        //     } else {
-        //         resolve(results[0]);
-        //     }
-        // });
         try {
             const usuario = await Usuario.findByPk(id);
             if (!usuario)  reject('Cliente no encontrado');
@@ -25,16 +16,7 @@ exports.getClienteById = async (id) => {
 exports.createCliente = async (clienteData) => {
     const { nombre, email, password, phone } = clienteData;
     return new Promise(async(resolve, reject) => {
-        // db.query('INSERT INTO usuarios (nombre, email, password, phone) VALUES (?, ?, ?, ?)', [nombre, email, password, phone], (err, result) => {
-        //     if (err) {
-        //         reject(err);
-        //     } else {
-        //         resolve({ id: result.insertId, nombre, email, password, phone });
-        //     }
-        // });
         try {
-            // const usuario = await Usuario.create(clienteData);
-            // console.log(bcrypt.(password))
             resolve(await Usuario.create({nombre, email, password, phone }));
         } catch (err) {
             reject(err);
@@ -44,13 +26,6 @@ exports.createCliente = async (clienteData) => {
 
 exports.getAllClientes = async () => {
     return new Promise(async (resolve, reject) => {
-        // db.query('SELECT id, nombre, email, password, phone FROM usuarios', (err, results) => {
-        //     if (err) {
-        //         reject(err);
-        //     } else {
-        //         resolve(results);
-        //     }
-        // });
         try {
             const usuarios = await Usuario.findAll();
             if (!usuarios)  reject('Cliente no encontrado');
@@ -64,13 +39,6 @@ exports.getAllClientes = async () => {
 exports.updateCliente = async (id, clienteData) => {
     const { nombre, email, password, phone } = clienteData;
     return new Promise(async (resolve, reject) => {
-        // db.query('UPDATE usuarios SET nombre = ?, email = ?, password = ?, phone = ? WHERE id = ?', [nombre, email, password, phone, id], (err) => {
-        //     if (err) {
-        //         reject(err);
-        //     } else {
-        //         resolve({ id, nombre, email, password, phone });
-        //     }
-        // });
         try {
             const usuario = await Usuario.findByPk(id);
             if (!usuario) throw new Error('Cliente no encontrado');
@@ -85,13 +53,6 @@ exports.updateCliente = async (id, clienteData) => {
 
 exports.deleteCliente = async (id) => {
     return new Promise(async (resolve, reject) => {
-        // db.query('DELETE FROM usuarios WHERE id = ?', [id], (err) => {
-        //     if (err) {
-        //         reject(err);
-        //     } else {
-        //         resolve();
-        //     }
-        // });
         try {
             const usuario = await Usuario.findByPk(id);
             if (!usuario) throw new Error('Cliente no encontrado');
